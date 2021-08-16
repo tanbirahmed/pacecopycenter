52a541d3e690becd2a585470f176f20a3a02f31e
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2021-08-15 22:01:14 -0400

    css grid and hover effect'

---
+++ script.js
@@ -0,0 +1,156 @@
+(function () {
+  // Init
+  var cont = document.getElementById("cont"),
+    inner = document.getElementById("inner");
+
+  // Mouse
+  var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function (event) {
+      var e = event || window.event;
+      this.x = e.clientX - this._x;
+      this.y = (e.clientY - this._y) * -1;
+    },
+    setOrigin: function (e) {
+      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function () {
+      return "(" + this.x + ", " + this.y + ")";
+    },
+  };
+
+  // Track the mouse position relative to the center of the container.
+  mouse.setOrigin(cont);
+
+  //-----------------------------------------
+
+  var counter = 0;
+  var updateRate = 10;
+  var isTimeToUpdate = function () {
+    return counter++ % updateRate === 0;
+  };
+
+  //-----------------------------------------
+
+  var onMouseEnterHandler = function (event) {
+    update(event);
+  };
+
+  var onMouseLeaveHandler = function () {
+    inner.style = "";
+  };
+
+  var onMouseMoveHandler = function (event) {
+    if (isTimeToUpdate()) {
+      update(event);
+    }
+  };
+
+  //-----------------------------------------
+
+  var update = function (event) {
+    mouse.updatePosition(event);
+    updateTransformStyle(
+      (mouse.y / inner.offsetHeight / 2).toFixed(2),
+      (mouse.x / inner.offsetWidth / 2).toFixed(2)
+    );
+  };
+
+  var updateTransformStyle = function (x, y) {
+    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    inner.style.transform = style;
+    inner.style.webkitTransform = style;
+    inner.style.mozTransform = style;
+    inner.style.msTransform = style;
+    inner.style.oTransform = style;
+  };
+
+  //-----------------------------------------
+
+  cont.onmouseenter = onMouseEnterHandler;
+  cont.onmouseleave = onMouseLeaveHandler;
+  cont.onmousemove = onMouseMoveHandler;
+})();
+
+//NEWWWWWWWW///
+(function () {
+  // Init
+  var cont1 = document.getElementById("cont1"),
+    inner1 = document.getElementById("inner1");
+
+  // Mouse
+  var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function (event) {
+      var e = event || window.event;
+      this.x = e.clientX - this._x;
+      this.y = (e.clientY - this._y) * -1;
+    },
+    setOrigin: function (e) {
+      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function () {
+      return "(" + this.x + ", " + this.y + ")";
+    },
+  };
+
+  // Track the mouse position relative to the center of the container.
+  mouse.setOrigin(cont1);
+
+  //-----------------------------------------
+
+  var counter = 0;
+  var updateRate = 10;
+  var isTimeToUpdate = function () {
+    return counter++ % updateRate === 0;
+  };
+
+  //-----------------------------------------
+
+  var onMouseEnterHandler = function (event) {
+    update(event);
+  };
+
+  var onMouseLeaveHandler = function () {
+    inner1.style = "";
+  };
+
+  var onMouseMoveHandler = function (event) {
+    if (isTimeToUpdate()) {
+      update(event);
+    }
+  };
+
+  //-----------------------------------------
+
+  var update = function (event) {
+    mouse.updatePosition(event);
+    updateTransformStyle(
+      (mouse.y / inner1.offsetHeight / 2).toFixed(2),
+      (mouse.x / inner1.offsetWidth / 2).toFixed(2)
+    );
+  };
+
+  var updateTransformStyle = function (x, y) {
+    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    inner1.style.transform = style;
+    inner1.style.webkitTransform = style;
+    inner1.style.mozTransform = style;
+    inner1.style.msTransform = style;
+    inner1.style.oTransform = style;
+  };
+
+  //-----------------------------------------
+
+  cont1.onmouseenter = onMouseEnterHandler;
+  cont1.onmouseleave = onMouseLeaveHandler;
+  cont1.onmousemove = onMouseMoveHandler;
+})();
